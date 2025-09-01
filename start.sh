79695fae1dc826f974ecc989af6cd1c9ecb1362c
Carlos <thecarlospcok@gmail.com>
2025-09-01 00:36:18 -0600

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-# Script para iniciar el bot en Termux
-
-termux-wake-lock
-cd ~/black-clover-MD
-tmux new -d -s blackclover "npm start"
-echo "✅ Bot iniciado en segundo plano usando tmux"
