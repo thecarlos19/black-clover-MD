8bebb41830bf9de82e755d0721a67e8138234def
Carlos <thecarlospcok@gmail.com>
2025-10-06 13:22:37 -0600

    Add files via upload

--- núcleo•clover/start.js
+++ núcleo•clover/start.js
@@ -478,7 +478,7 @@ setInterval(async () => {
   if (stopped === 'close' || !conn || !conn?.user) return;
   const _uptime = process.uptime() * 1000;
   const uptime = clockString(_uptime);
-  const bio = `🦠 Black-clover-MD |「🕒」Aᥴ𝗍і᥎ᥲ: ${uptime}`;
+  const bio = `🦠 Black-clover-MD |「🕒」Aᥴ𝗍і᥎o: ${uptime}`;
   await conn?.updateProfileStatus(bio).catch((_) => _);
 }, 60000);
 
