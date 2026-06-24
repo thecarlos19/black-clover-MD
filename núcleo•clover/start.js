cb80877cae2983e5f006cbc12a83d96186673db1
I'am Diego <sarmientodieg@gmail.com>
2026-06-24 18:41:28 -0500

    Actualizar start.js

--- núcleo•clover/start.js
+++ núcleo•clover/start.js
@@ -1,6 +1,6 @@
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1'
 import './config.js'
-import web from './lib/web.js'
+import web from '../lib/web.js'
 import cluster from 'cluster'
 const { setupMaster, fork } = cluster
 import { watchFile, unwatchFile } from 'fs'
